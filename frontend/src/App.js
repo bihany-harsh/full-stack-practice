36d60b9613b35ce9f6b9ebffc969d93bacbf2418
bihany-harsh <harshbihany7@gmail.com>
2023-03-07 23:13:39 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/Header";
+import NotesListPage from "./pages/NotesListPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NotePage from "./pages/NotePage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/" exact component={NotesListPage} />
+        <Route path="/note/:id" component={NotePage} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
